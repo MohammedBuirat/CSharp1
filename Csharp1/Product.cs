27e58e5208378d45d6270a42f2683f5d0aede947
Mohammed Buirat <1192896@student.birzeit.edu>
2023-08-09 03:22:45 +0300

    Implement Product

--- Csharp1/Product.cs
+++ Csharp1/Product.cs
@@ -8,5 +8,37 @@ namespace Csharp1
 {
     internal class Product
     {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+
+        public Product(String Name, decimal Price, int Quantity)
+        {
+            this.Name = Name;
+            this.Price = Price;
+            this.Quantity = Quantity;
+        }
+
+        public Product() { }
+
+        public override String ToString()
+        {
+            return $"Name: {Name}   price: {Price}    quantity:{Quantity}";
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj.GetType != GetType || obj == null) return false;
+            Product other = obj as Product;
+            if (Name == other.Name &&
+                Quantity == other.Quantity &&
+                Price == other.Price)
+            {
+                return true;
+            }
+            return false;
+
+        }
+
     }
 }
